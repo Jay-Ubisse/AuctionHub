9e2058cd1962ef6e7081b951e5605b24dc67d174
Jay Ubisse <ubisse.joaquim@outlook.com>
2022-04-13 05:25:24 +0200

    raplace Die with try-catch

--- assets/scripts/php/db_connection/connect.php
+++ assets/scripts/php/db_connection/connect.php
@@ -1,9 +1,12 @@
 <?php
     require('./config.php');
-
-    $dbcon = new mysqli(SERVER_NAME, USER_NAME, PASSWORD, DATA_BASE);
-    if ($dbcon->connect_error) {
-        die("Connection failed: " . $dbcon->connect_error);
+    try {
+        $dbcon = new mysqli(SERVER_NAME, USER_NAME, PASSWORD, DATA_BASE);
+    } catch (Exception $e) {
+        print "Connection failed: " . $e->getMessage();
+    }
+    catch (Error $e) {
+        print "Connection failed: " . $e->getMessage();
     }
     
 ?>
